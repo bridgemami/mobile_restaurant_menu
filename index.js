7f0f0744f46880b4fa5919401ee51d4f4c3a3148
Michael <mdbridgeman@gmail.com>
2023-08-10 15:49:28 -0700

    finished the remove item

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ let order = []
 document.addEventListener("click", function (e) {
   ;
   if (e.target.dataset.add) {
-    handleAddingProduct(e.target.dataset.add);
+    console.log(e.target.dataset.add)
+    handleAddingToTheOrder(e.target.dataset.add);
   } 
   else if (e.target.dataset.remove) {
-    handleAddingProduct(e.target.dataset.remove);
+    handleDeletingFromTheOrder(e.target.dataset.remove);
   }
   });
 
@@ -19,3 +20,79 @@ function addSpace(arr) {
   return arr;
 }
 
+function handleAddingToTheOrder (id) {
+  const targetMenuItem = menuArray.filter(item => item.id === Number(id))[0];
+  addToTheOrder(targetMenuItem)
+   renderOrder()
+}
+
+function addToTheOrder (item) {
+  order.push({...item})
+}
+
+function handleDeletingFromTheOrder (id) {
+  const targetMenuItemIndex = getIndexFromOrder(id)
+  console.log(targetMenuItemIndex)
+  removeItemFromOrder(targetMenuItemIndex)
+  
+  renderOrder()
+}
+
+function removeItemFromOrder(index) {
+  console.log(index)
+  order.splice(index, 1);
+}
+
+function getIndexFromOrder(id) {
+    return order.findIndex((item) => item.id === Number(id));
+  }
+  
+
+function renderMenu() {
+  const menuEl = document.getElementById("menu");
+  let menuHtml = ``
+  menuArray.forEach(food => {
+    menuHtml += `
+        <section class="food-container">
+
+        <div>
+        <h2>${food.emoji}</h2>
+        </div>
+
+        <div class='menu-description'>
+        <h3>${food.name}</h3>
+        <p>${addSpace(food.ingredients)}</p>
+        <h5>$${food.price}</h5>
+        </div>
+
+        
+        <button class="add" data-add="${food.id}">+</button>
+        
+        </section>`;
+  });
+  menuEl.innerHTML = menuHtml;
+}
+renderMenu()
+
+function renderOrder() {
+  const orderEl =document.getElementById('order'); 
+  // let orderItemsHtml = `` 
+  orderEl.innerHTML = `<h2>Your Order</h2>
+                      <div>${ renderOrderItem()}</div>
+                      <hr />
+                      <h5>Total:</h5>
+                      <p>$</p>
+                      <button id="order-btn">Complete Your Order</button>`
+
+}
+
+function renderOrderItem () {
+  let orderItemsHtml = ``
+  order.forEach((item) => {
+   orderItemsHtml += `<p>${item.name}</p>
+          <button class="remove" data-remove="${item.id}">Remove</button>
+          <p>$${item.price}</p>
+          ` 
+})
+return orderItemsHtml
+}
